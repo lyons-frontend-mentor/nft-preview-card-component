c9f81a54c4966b26a64a22f9d0aa10fe27131891
Greg Lyons <gregory.lyons512@gmail.com>
2022-05-03 15:24:00 -0500

    Finished HTML and added custom colors to Tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        softblue: "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        darkbluebg: "hsl(217, 54%, 11%)",
+        darkbluecard: "hsl(216, 50%, 16%)",
+        darkblueline: "hsl(215, 32%, 27%)"
+      }
+    },
   },
   plugins: [],
 }
